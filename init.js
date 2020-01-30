3b826e32466c7353b13903ee6ef1eb1c6b9571c7
Eliot Waddell <eliotwaddell99@gmail.com>
2020-01-29 20:14:21 -0600

    draw calculator

---
+++ init.js
@@ -0,0 +1,24 @@
+function operate(operator, num1, num2)
+{
+    if(operator == "+")
+    {
+        return num1+num2;
+    }
+    else if(operator == "-")
+    {
+        return num1-num2;
+    }
+    else if(operator == "*")
+    {
+        return num1*num2;
+    }
+    else if(operator == "/")
+    {
+        return num1/num2;
+    }
+}
+
+function expression(expr)
+{
+    
+}
