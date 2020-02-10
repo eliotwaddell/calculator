cb2736be2874ebd837bca90b8f122010c537c7df
Eliot Waddell <eliotwaddell99@gmail.com>
2020-02-09 21:27:14 -0600

    added exponents, started negative numbers

--- init.js
+++ init.js
@@ -4,13 +4,14 @@ var SIGDIG = 10000000;//for fixing float problems
 var userInput = '';//input to be filtered and displayed
 var pointUsed = false; //this is so user can't use more than one decimal per item
 var opUsed = false;//to avoid double operators
+var negative = false;//to track negative numbers
 
 function floatFix(fl)
 {
     return Math.floor((fl)*SIGDIG)/SIGDIG;//truncates any tiny digits
 }
 
-function operate(operator, num1, num2)
+function operate(operator, num1, num2)//operates independent instructions
 {
     if(operator == "+")//self explanatory
     {
@@ -18,7 +19,7 @@ function operate(operator, num1, num2)
     }
     else if(operator == "-")
     {
-        return num1-num2;
+        return num2*(-1);
     }
     else if(operator == "*")
     {
@@ -26,25 +27,37 @@ function operate(operator, num1, num2)
     }
     else if(operator == "/")
     {
+        if(num2 == 0)
+        {
+            return 'ERROR';
+        }
         return num1/num2;
+    }
+    else if(operator == "^")
+    {
+        return num1**num2;
     }
 }
 
-function expression(expr)
+function expression(expr)//evaluates entire user input field
 {
     if(!(expr.charAt(expr.length-1) <= '9' && expr.charAt(expr.length-1) >= '0'))//if the expression doesn't end with a number, chop off last operator
     {
-        expr = expr.slice(length-1,length);
+        expr = expr.substr(0,expr.length-1);
     }
     var regex = /\d+(\.\d+)?/g;//regular expression
-    var opRegex = /[+\-*/]/g;
+    var opRegex = /[+\-*/^]/g;//all of the operators
     var numbers = expr.match(regex).map(function(v) { return parseFloat(v) });//shoutout stackoverflow, maps each parsed "float" string to a float
     var ops = expr.match(opRegex);//no need to map, ops are strings anyway
     var index = 0;//two tracking variables
     var result = numbers[0];//initialize in case of 1 number input
     while(numbers.length>1)//as long as there are two numbers, they can be operated on
     {
-        if(ops.includes('*'))
+        if(ops.includes('-') && ops.length != numbers.length-1)//this is ONLY for negative numbers
+        {//this means there are extra ops which are only logically allowed to be negative signs
+
+        }
+        else if(ops.includes('*'))
         {
             index = ops.indexOf('*');
             result = operate('*', numbers[index], numbers[index+1])//operates on elements sandwiching op
@@ -56,9 +69,15 @@ function expression(expr)
         {
             index = ops.indexOf('/');
             result = operate('/', numbers[index], numbers[index+1])//operates on elements sandwiching op
+            if(result == 'ERROR')
+            {
+                userInput = 'DIV/0!';
+                return;
+            }
             ops.splice(index,1);//cuts out the used operator
             numbers[index+1] = result;//appends result to right side of used operation
             numbers.splice(index,1);//cuts out left side of used operation;
+
         }
         else
         {
@@ -72,13 +91,11 @@ function expression(expr)
     result = floatFix(result);
     if(!(result>99999999))
     {
-        display.textContent = result.toString();
         userInput = result.toString();
     }
     else
     {
-        display.textContent = "OVERFLOW";
-        userInput = '';
+        userInput = 'OVERFLOW';
     }
     pointUsed = false;//clean slate
     opUsed = false;
@@ -100,28 +117,29 @@ function addToDisplay(newInput)//filters incoming input
         pointUsed = true;//no double points
         return;
     }
-    if(['+','-','*','/'].indexOf(newInput) != -1)//if the input is an operator
+    if(['+','-','*','/','^'].indexOf(newInput) != -1)//if the input is an operator
     {
-        if(userInput.length == 0)
+        if(userInput.length == 0 || opUsed)
         {
-            if(newInput == '-')
+            if(newInput == '-' && !negative)//negative numbers
             {
-                userInput += newInput;
-                opUsed = true;
-                pointUsed = false;
+                negative = true;
             }
-            return;
+            else
+            {
+                return;
+            }
         }
-        if(opUsed)
-        {
-            return;
+        if(userInput.charAt(userInput.length-1) == '.')
+        {//if the previous number's decimal was left hanging
+            userInput += '0';//add a zero
         }
         userInput += newInput;
         opUsed = true;
         pointUsed = false;
         return;
     }
-    opUsed = false;
+    opUsed = false;//case of number input
     userInput += newInput;
     return;
 
@@ -163,9 +181,13 @@ for (var x of buttons)//iterates through the input buttons to allow them to be u
         {
             expression(userInput);
             update();
+            if(userInput == 'OVERFLOW' || userInput == 'DIV/0!')//clears the screen if overflow reached/divided by 0
+            {
+                userInput = '';
+            }
         });
     }
-    if(x.id == 'C')
+    if(x.id == 'C')//backspace
     {
         x.addEventListener("click", function(e)
         {
@@ -173,11 +195,18 @@ for (var x of buttons)//iterates through the input buttons to allow them to be u
             update();
         });
     }
-    if(x.id == 'SQRT')
+    if(x.id == 'SQRT')//square root the current display
     {
         x.addEventListener("click", function(e)
         {
 
+        });
+    }
+    if(x.id == '^')//allows user to input exponent
+    {
+        x.addEventListener("click", function(e)
+        {
+            
         });
     }
 }
